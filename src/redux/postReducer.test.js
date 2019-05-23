527fc30857d4cfa1208c80d841729f13d237c8a2
Matt Groberg <grobergmg@gmail.com>
2019-05-23 08:15:04 -0700

    add post reducers and initial test

--- src/redux/postReducer.test.js
+++ src/redux/postReducer.test.js
@@ -1,3 +1,20 @@
-import postReducer from './postReducers';
+import postList from './postReducers';
 import c from './../constants';
 
+const initialState={
+	'hjkhskhjskhksh':{
+		title:'Sample Post',
+		content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis dui eget nisl aliquam feugiat. Integer sit amet erat aliquam, rhoncus nibh eget, tincidunt purus. Nullam varius tincidunt pulvinar',
+		upVotes:0,
+		downVotes:0
+		}
+	}
+
+
+
+describe('Post Reducers',()=>{
+	test('return initial state if unkown action type',()=>{
+		expect(postList(initialState,{type:null})).toEqual(initialState);
+	})
+
+})
--- src/redux/postReducers.js
+++ src/redux/postReducers.js
@@ -1,8 +1,38 @@
 import c from './../constants';
 
+
+const post=(state,action)=>{
+	switch(action.types){
+		case c.UP_VOTE:
+			if (action.id!==state.id){
+				return state;
+			}
+			return Object.assign({},state,{upVotes:state.upVotes+1})
+		case c.DOWN_VOTE:
+			if (action.id!==state.id){
+				return state;
+			}
+			return Object.assign({},state,{downVotes:state.downVotes+1})
+		default:
+			return state;
+	}
+}
+
 const postList=(state,action)=>{
 	switch(action.type){
 		case c.ADD_POST:
-			return 
+			return Object.assign({},state,{[action.id]:action.newPost})
+		case c.UP_VOTE:
+			return state.keys.map(id=>{
+				return post(state[id],action)
+			})
+		case c.DOWN_VOTE:
+			return state.keys.map(id=>{
+				return post(state[id],action)
+			})
+		default:
+			return state;
 	}
 }
+
+export default postList;
