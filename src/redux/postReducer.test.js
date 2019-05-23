cf5fbea6d7ae44edf52c93255d425f45fe0e1bcb
Matt Groberg <grobergmg@gmail.com>
2019-05-23 08:30:39 -0700

    test ADD_POST action

--- src/redux/postReducer.test.js
+++ src/redux/postReducer.test.js
@@ -16,5 +16,18 @@ describe('Post Reducers',()=>{
 	test('return initial state if unkown action type',()=>{
 		expect(postList(initialState,{type:null})).toEqual(initialState);
 	})
+test('add new post to list',()=>{
+	const action={
+		type:'ADD_POST',
+		id:'aassalsasdad',
+		newPost:{
+			title:'Sample2',
+			content:'lorem lorem lorem',
+			upVotes:0,
+			downVotes:0}
+		}
+	const nextState=Object.assign({},initialState,{[action.id]:action.newPost});
+		expect(postList(initialState,action)).toEqual(nextState);
+	})
 
 })
